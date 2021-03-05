1b1e7f71dc238bc9bf300001b703ab6ed1822285
kanga333 <e411z7t40w@gmail.com>
2021-03-05 17:16:54 +0900

    Set up Mock simply

--- tests/test_dimensions.py
+++ tests/test_dimensions.py
@@ -5,18 +5,14 @@ import cepan
 
 def test_get_dimensions(mocker):
     client_mock = mocker.Mock()
-    client_mock.get_dimension_values = mocker.Mock(
-        return_value={
-            "DimensionValues": [
-                {"Value": "", "Attributes": {}},
-                {"Value": "ap-northeast-1", "Attributes": {}},
-            ],
-        }
-    )
-    mocker.patch(
-        "boto3.client",
-        return_value=client_mock,
-    )
+    client_mock.get_dimension_values.return_value = {
+        "DimensionValues": [
+            {"Value": "", "Attributes": {}},
+            {"Value": "ap-northeast-1", "Attributes": {}},
+        ],
+    }
+    mocker.patch("boto3.client", return_value=client_mock)
     start = datetime.date(2020, 1, 1)
     df = cepan.get_dimension_values("AZ", start)
-    assert len(df) == 1
+    assert len(df.index) == 1
+    assert len(df.columns) == 2
