62bbeaf9a89f7fa12ef08bcea469ab2be7ddc334
kanga333 <e411z7t40w@gmail.com>
2021-03-09 21:45:54 +0900

    Rename file

--- cepan/__init__.py
+++ cepan/__init__.py
@@ -1,5 +1,5 @@
 from cepan.cost_and_usage import get_cost_and_usage
-from cepan.dimensions import get_dimension_values, get_dimensions
+from cepan.dimension import get_dimension_values, get_dimensions
 
 __all__ = [
     "get_dimensions",
