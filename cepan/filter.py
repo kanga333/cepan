b11e074941c759f3961c289aec7006ce1c2a1d7b
kanga333 <e411z7t40w@gmail.com>
2021-03-12 20:07:32 +0900

    Add filter classes

---
+++ cepan/filter.py
@@ -0,0 +1,61 @@
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional, Protocol
+
+
+class Filter(Protocol):
+    def build_expression(self) -> Dict[str, Any]:
+        ...
+
+
+@dataclass
+class _BaseFilter:
+    key: str
+    values: List[str]
+    match_options: Optional[List[str]] = None
+
+    def _build_base_expression(self) -> Dict[str, Any]:
+        filter: Dict[str, Any] = {"Key": self.key, "Values": self.values}
+        if self.match_options:
+            filter["MatchOptions"] = self.match_options
+        return filter
+
+
+class Dimensions(_BaseFilter):
+    def build_expression(self) -> Dict[str, Any]:
+        return {"Dimensions": self._build_base_expression()}
+
+
+class Tags(_BaseFilter):
+    def build_expression(self) -> Dict[str, Any]:
+        return {"Tags": self._build_base_expression()}
+
+
+class CostCategories(_BaseFilter):
+    def build_expression(self) -> Dict[str, Any]:
+        return {"CostCategories": self._build_base_expression()}
+
+
+@dataclass
+class _CompositeFilter:
+    filters: List[Filter]
+
+    def _build_composite_expression(self) -> List[Dict[str, Any]]:
+        return [f.build_expression() for f in self.filters]
+
+
+class And(_CompositeFilter):
+    def build_expression(self) -> Dict[str, Any]:
+        return {"And": self._build_composite_expression()}
+
+
+class Or(_CompositeFilter):
+    def build_expression(self) -> Dict[str, Any]:
+        return {"Or": self._build_composite_expression()}
+
+
+@dataclass
+class Not:
+    filter: Filter
+
+    def build_expression(self) -> Dict[str, Any]:
+        return {"Not": self.filter.build_expression()}
---
+++ tests/test_filter.py
@@ -0,0 +1,115 @@
+import pytest
+from cepan.filter import (
+    And,
+    _BaseFilter,
+    _CompositeFilter,
+    CostCategories,
+    Dimensions,
+    Not,
+    Or,
+    Tags,
+)
+
+
+@pytest.mark.parametrize(
+    "args,expected",
+    [
+        (
+            {"key": "key", "values": ["value1", "value2"]},
+            {"Key": "key", "Values": ["value1", "value2"]},
+        ),
+        (
+            {"key": "key", "values": ["value1"], "match_options": ["EQUALS"]},
+            {"Key": "key", "Values": ["value1"], "MatchOptions": ["EQUALS"]},
+        ),
+    ],
+)
+def test_basefilter(args, expected):
+    f = _BaseFilter(**args)
+    assert f._build_base_expression() == expected
+
+
+def test_dimensions():
+    f = Dimensions("key", ["value1"])
+    assert f.build_expression() == {"Dimensions": {"Key": "key", "Values": ["value1"]}}
+
+
+def test_tags():
+    f = Tags("key", ["value1"])
+    assert f.build_expression() == {"Tags": {"Key": "key", "Values": ["value1"]}}
+
+
+def test_costcategories():
+    f = CostCategories("key", ["value1"])
+    assert f.build_expression() == {
+        "CostCategories": {"Key": "key", "Values": ["value1"]}
+    }
+
+
+@pytest.mark.parametrize(
+    "filters,expected",
+    [
+        (
+            [
+                Dimensions("key1", ["value1"]),
+            ],
+            [
+                {"Dimensions": {"Key": "key1", "Values": ["value1"]}},
+            ],
+        ),
+        (
+            [
+                Dimensions("key1", ["value1"]),
+                Dimensions("key2", ["value2"]),
+            ],
+            [
+                {"Dimensions": {"Key": "key1", "Values": ["value1"]}},
+                {"Dimensions": {"Key": "key2", "Values": ["value2"]}},
+            ],
+        ),
+        (
+            [
+                Dimensions("key1", ["value1"]),
+                And(
+                    [
+                        Dimensions("key2", ["value2"]),
+                        Dimensions("key3", ["value3"]),
+                    ]
+                ),
+            ],
+            [
+                {"Dimensions": {"Key": "key1", "Values": ["value1"]}},
+                {
+                    "And": [
+                        {"Dimensions": {"Key": "key2", "Values": ["value2"]}},
+                        {"Dimensions": {"Key": "key3", "Values": ["value3"]}},
+                    ]
+                },
+            ],
+        ),
+    ],
+)
+def test_compositefilter(filters, expected):
+    f = _CompositeFilter(filters)
+    assert f._build_composite_expression() == expected
+
+
+def test_and():
+    f = And([Dimensions("key", ["value"])])
+    assert f.build_expression() == {
+        "And": [{"Dimensions": {"Key": "key", "Values": ["value"]}}]
+    }
+
+
+def test_or():
+    f = Or([Dimensions("key", ["value"])])
+    assert f.build_expression() == {
+        "Or": [{"Dimensions": {"Key": "key", "Values": ["value"]}}]
+    }
+
+
+def test_not():
+    f = Not(Dimensions("key", ["value"]))
+    assert f.build_expression() == {
+        "Not": {"Dimensions": {"Key": "key", "Values": ["value"]}}
+    }
