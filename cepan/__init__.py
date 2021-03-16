17c63b94ccff467bcf2062d1872c8bf836f5801d
kanga333 <e411z7t40w@gmail.com>
2021-03-16 21:56:11 +0900

    Export classes to use as arguments

--- cepan/__init__.py
+++ cepan/__init__.py
@@ -1,8 +1,19 @@
-from cepan.cost_and_usage import get_cost_and_usage
-from cepan.dimension import get_dimension_values, get_dimensions
+from cepan._cost_and_usage import get_cost_and_usage
+from cepan._dimension import get_dimension_values, get_dimensions
+from cepan._filter import And, CostCategories, Dimensions, Not, Or, Tags
+from cepan._group_by import GroupBy
+from cepan._time_period import TimePeriod
 
 __all__ = [
     "get_dimensions",
     "get_dimension_values",
     "get_cost_and_usage",
+    "TimePeriod",
+    "Dimensions",
+    "Tags",
+    "CostCategories",
+    "And",
+    "Or",
+    "Not",
+    "GroupBy",
 ]
